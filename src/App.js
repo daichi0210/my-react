225f6c085030cae727d2481c9bb6b9e7a02d8481
daichi <kabata0210@icloud.com>
2025-04-08 21:21:47 +0900

    tutorial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 const products = [
-  { title: "Cabbage", id: 1 },
-  { title: "Garlic", id: 2 },
-  { title: "Apple", id: 3 },
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
 ];
 
-const listItems = products.map((product) => (
-  <li key={product.id}>{product.title}</li>
-));
-
 export default function MyApp() {
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{ color: product.isFruit ? "magenta" : "darkgreen" }}
+    >
+      {product.title}
+    </li>
+  ));
+
   return <ul>{listItems}</ul>;
 }

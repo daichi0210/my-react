3f271c5aa2c7174a092f2bf417802c544c959a8e
daichi <kabata0210@icloud.com>
2025-04-09 22:09:38 +0900

    フックの使用とデータの共有

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { useState } from "react";
 
-function MyButton() {
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
+
+export default function MyApp() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
   }
 
-  return <button onClick={handleClick}>Clicked {count} times</button>;
-}
-
-export default function MyApp() {
   return (
     <div>
       <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }

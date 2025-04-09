86d5b4d8644e07fb4403e513515e64f7f944b7f2
daichi <kabata0210@icloud.com>
2025-04-09 22:05:05 +0900

    イベントと画面の更新

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-const products = [
-  { title: "Cabbage", isFruit: false, id: 1 },
-  { title: "Garlic", isFruit: false, id: 2 },
-  { title: "Apple", isFruit: true, id: 3 },
-];
+import { useState } from "react";
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
 
 export default function MyApp() {
-  const listItems = products.map((product) => (
-    <li
-      key={product.id}
-      style={{ color: product.isFruit ? "magenta" : "darkgreen" }}
-    >
-      {product.title}
-    </li>
-  ));
-
-  return <ul>{listItems}</ul>;
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
 }
